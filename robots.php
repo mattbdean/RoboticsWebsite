fd26b4c20e18a8cca1a2b764361a3dd4509a9822
scarca <rajshrimali@gmail.com>
2013-09-11 06:55:28 -0400

    Provision for previous commit
    Makes sure no error occurs by making the file exist.

---
+++ robots.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>BWHS Robotics | Home</title>
+  <link rel="stylesheet" type="text/css" href="styles/global.css">
+  <link rel="stylesheet" type="text/css" href="styles/header.css">
+  <link rel="stylesheet" type="text/css" href="styles/footer.css">
+  <!-- Google Fonts: Open Sans -->
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+  <!-- Google Fonts: Aldrich -->
+  <link href='http://fonts.googleapis.com/css?family=Aldrich' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <div id="wrapper">
+   <?php
+    include 'template/header.php';
+    ?>
+    <!-- filler content not included --> 
+	<?php
+    include 'template/footer.php';
+    ?>
+</div>
+</body>
+</html>
+
