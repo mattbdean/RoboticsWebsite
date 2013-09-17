b1376a4d00d77a270081d914cbeb7b5d6f1cdda3
Matthew Dean <deanmatthew16@gmail.com>
2013-09-16 21:21:23 -0400

    Forgot to close the div

--- news.php
+++ news.php
@@ -102,9 +102,10 @@
 				} else {
 					// <p>5 (fb like icon), 10 (fb comment icon)</p>
 					printf('<p>%s %s, %s comments</p>', $likes == 0 ? "No" : $likes, $likes != 0 ? '<img src="res/fb_like.png" class="fb-icon" alt=" likes">' : "likes", $comments);
-					print '</div>';
 				}
 				
+				print '</div>';
+
 
 				// End fb-post
 				print '</div><hr>';
